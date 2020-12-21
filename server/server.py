8c84418acea7a111e69407272eaa139ad756fcdc
n-majdou <majdoubnarjes@gmail.com>
2020-12-21 14:44:40 +0100

    fix deploy1

--- server/server.py
+++ server/server.py
@@ -1,7 +1,7 @@
 # server manages the routing of request and response
 from flask import Flask, request, jsonify, render_template
-import util
-#import server.util as util
+#import util
+import server.util as util
 
 app = Flask(__name__, static_url_path='/client', static_folder='../client', template_folder='../client')
 
